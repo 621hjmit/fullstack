cbcc7ebc82948173f6d3eb1ab7edff0496c8a22d
superant621 <superant621@gmail.com>
2024-12-04 13:31:47 +0900

    fff

--- src/main/java/com/shop/fullstack/common/config/WebConfig.java
+++ src/main/java/com/shop/fullstack/common/config/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /*
+  
     @Value("${file.upload.path}")
     private String uploadPath;
     
@@ -14,5 +16,16 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///" + uploadPath);
-    }
+    }*/
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      // /static/ 경로로 접근 시 classpath:/static/ 폴더의 파일을 제공
+      registry.addResourceHandler("/static/**")
+              .addResourceLocations("classpath:/static/");
+
+      // /uploads/ 경로로 접근 시 file:/var/www/uploads/ 폴더의 파일을 제공
+      registry.addResourceHandler("/uploads/**")
+              .addResourceLocations("file:/var/www/uploads/");
+  }
 }
